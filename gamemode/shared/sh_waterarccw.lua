7bc967407e36bd64f69c80805537f5fbf00fe90f
Pathfinder_FUFU <49439351+server00987@users.noreply.github.com>
2022-03-04 20:50:04 +0800

    Beta 1.1.3

--- gamemode/shared/sh_waterarccw.lua
+++ gamemode/shared/sh_waterarccw.lua
@@ -1,20 +1,20 @@
-----------------------------------------------
+------------------------------------------------
 --Thanks to Nano_Rex ◥◣ム◢◤ for their help--|
-----------------------------------------------
+------------------------------------------------
 
 -- timer needed because weplist is loaded after the gamemod is loaded
 
-timer.Simple(0.1, function()
+timer.Simple(0.1,function()
 	local ConVar = GetConVar("flood_arccw_shootunderwater"):GetFloat()
 
 	if Flood_Weapons then
 		for k, tbl in pairs(Flood_Weapons) do
 			local wep = weapons.GetStored(tbl.Class)
-
 			if not wep then continue end
+
 			if not string.find(wep.Base, "arccw") then continue end
 
-			if ConVar >= 1 then
+			if ConVar >= 1 then 
 				wep.CanFireUnderwater = 1
 			end
 		end
