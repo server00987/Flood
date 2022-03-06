d1437c009598f529023e6abf2d0eea6349b85126
Pathfinder_FUFU <49439351+server00987@users.noreply.github.com>
2022-03-06 13:35:22 +0800

    Beta 1.1.6

---
+++ gamemode/languages/german.lua
@@ -0,0 +1,86 @@
+--[[
+Valid languages (from gmod's menu): bg cs da de el en en-PT es-ES et fi fr ga-IE he hr hu it ja ko lt nl no pl pt-BR pt-PT ru sk sv-SE th tr uk vi zh-CN zh-TW
+You must use one of the above when using translate.AddLanguage
+
+Flood Translation Guideline
+1. The language that be translated is your native language.
+2. Only translate formally. Do not translate with slang, improper grammar, spelling, etc.
+3. Do not translate code characters. (e.g. %s /n)
+4. Please use English instead if untranslated or untranslatable.
+--]]
+ 
+translate.AddLanguage("de", "German")
+ 
+-- HUD
+LANGUAGE["HUD_Waiting_for_players"] = "Warte auf Spieler"
+LANGUAGE["HUD_Build_a_boat"] = "Bau ein Boot"
+LANGUAGE["HUD_Get_on_your_boat"] = "Geh auf dein Boot"
+LANGUAGE["HUD_Destroy_enemy_boats"] = "Zerstöre gegnerische Boote"
+LANGUAGE["HUD_Restarting_the_round"] = "Runden-neustart"
+LANGUAGE["HUD_Name"] = "Name: "
+LANGUAGE["HUD_Health"] = "Leben: "
+LANGUAGE["HUD_Unarmed"] = "Unbewaffnet"
+LANGUAGE["HUD_Youre_Dead"] = "Du bist Tod"
+LANGUAGE["HUD_killed"] = "getötet"
+ 
+-- NADMOD
+LANGUAGE["NADMOD_Owner"] = "Besitzer: "
+LANGUAGE["NADMOD_ISADMIN"] = "Warte auf den Server zur Admin bestätigung..."
+LANGUAGE["NADMOD_NADMOD_PP_Admin_Panel"] = "NADMOD PP Administrationsmenü"
+LANGUAGE["NADMOD_Main_PP_Power_Switch"] = "Main PP Stromschalter"
+LANGUAGE["NADMOD_Admins_can_touch_anything"] = "Admins können alles berühren"
+LANGUAGE["NADMOD_Use_Protection"] = "Schutz nutzen"
+LANGUAGE["NADMOD_Autoclean_Disconnected_Players"] = "Getrennte Spieler automatisch aufräumen?"
+LANGUAGE["NADMOD_Autoclean_Admins"] = "Admins automatisch aufräumen"
+LANGUAGE["NADMOD_Autoclean_Timer"] = "Automatisch aufräumen-Timer"
+LANGUAGE["NADMOD_Apply_Settings"] = "Einstellungen anwenden"
+LANGUAGE["NADMOD_Cleanup_Panel"] = "                     Aufräummenü"
+LANGUAGE["NADMOD_Cleanup_Disconnected_Players_Props"] = "Props von getrennten Spielern aufräumen ("
+LANGUAGE["NADMOD_Cleanup_All_NPCs"] = "Alle NPCs aufräumen"
+LANGUAGE["NADMOD_Cleanup_All_Ragdolls"] = "Alle Ragdolls aufräumen"
+LANGUAGE["NADMOD_Cleanup_Clientside_Ragdolls"] = "Clientside Ragdolls aufräumen"
+LANGUAGE["NADMOD_Client_Panel"] = "NADMOD - Spielermenü"
+LANGUAGE["NADMOD_Cleanup_Props"] = "Props aufräumen"
+LANGUAGE["NADMOD_Clear_Clientside_Ragdolls"] = "Clientside Ragdolls leeren"
+LANGUAGE["NADMOD_Friends_Panel"] = "                     Freundemenü"
+LANGUAGE["NADMOD_No_Other_Players_Are_Online"] = "Keine anderen Spieler sind Online"
+LANGUAGE["NADMOD_Apply_Friends"] = "Freunde anwenden"
+ 
+-- Shop
+LANGUAGE["SHOP_Props"] = "Props"
+LANGUAGE["SHOP_Weapons"] = "Waffen"
+LANGUAGE["SHOP_Tools"] = "Tools"
+LANGUAGE["SHOP_Prop_Protection"] = "Prop Protection"
+LANGUAGE["SHOP_ERROR"] = [[Fehler beim Laden der Prop-Kategorietabelle - bitte benachrichtigen Sie den Serverbetreiber.]]
+LANGUAGE["SHOP_Health"] = "\nLeben: "
+LANGUAGE["SHOP_Price"] = "\nPreis: €"
+LANGUAGE["SHOP_ERROR2"] = "Tooltip konnte nicht geladen werden – Beschreibung fehlt"
+LANGUAGE["SHOP_ERROR3"] = [[Prop-Tabelle konnte nicht geladen werden – bitte benachrichtigen Sie den Serverbetreiber.]]
+LANGUAGE["SHOP_Client_settings"] = "Client-Einstellungen"
+LANGUAGE["SHOP_Admin_settings"] = "Admin-Einstellungen"
+LANGUAGE["SHOP_ERROR4"] = [[Waffenkategorientabelle konnte nicht geladen werden - bitte benachrichtigen Sie den Serverbetreiber.]]
+LANGUAGE["SHOP_Damage"] = "\nSchaden: "
+LANGUAGE["SHOP_Ammo"] = "\nMunition: "
+LANGUAGE["SHOP_Floatability"] = "\nSchwimmfähigkeit: "
+LANGUAGE["SHOP_ERROR5"] = "Tooltip konnte nicht geladen werden – Beschreibung fehlt"
+LANGUAGE["SHOP_ERROR6"] = [[Fehler beim Laden der Waffentabelle - bitte benachrichtigen Sie den Serverbetreiber.]]
+LANGUAGE["SHOP_ERROR7"] = "Beim Laden Ihres Tools-Bereichs ist ein Fehler aufgetreten. Bitte treten Sie dem Server erneut bei oder wenden Sie sich an einen Administrator, um diesen Fehler zu beheben."
+ 
+-- Scoreboard
+LANGUAGE["Scoreboard_User"] = "Spieler"
+LANGUAGE["Scoreboard_Admin"] = "Admin"
+LANGUAGE["Scoreboard_SuperAdmin"] = "Super Admin"
+LANGUAGE["Scoreboard_Founder"] = "Gründer"
+LANGUAGE["Scoreboard_Owner"] = "Besitzer"
+LANGUAGE["Scoreboard_VIP"] = "VIP"
+LANGUAGE["Scoreboard_Donator"] = "Spender"
+LANGUAGE["Scoreboard_Name"] = "Name: "
+LANGUAGE["Scoreboard_Rank"] = "Rang: "
+LANGUAGE["Scoreboard_Cash"] = "Geld: "
+LANGUAGE["Scoreboard_Deaths"] = "Tode: "
+LANGUAGE["Scoreboard_Ping"] = "Ping: "
+LANGUAGE["Scoreboard_Players"] = "Spieler: "
+ 
+-- Cash Meta
+LANGUAGE["Meta_ERROR"] = "Error occured in AddCash function - No amount was passed."
+LANGUAGE["Meta_ERROR2"] = "Error occured in SubCash function - No amount was passed."
